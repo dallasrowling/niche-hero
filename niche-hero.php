5e916e546a717b2d98f2b7ba2353cfea3cc9c360
Dallas Rowling <djrowling94@gmail.com>
2021-02-15 10:46:54 -0700

    Alpha 3.0.0

--- niche-hero.php
+++ niche-hero.php
@@ -13,9 +13,15 @@ function niche_hero_shortcode( $atts = array() ) {
 	extract(shortcode_atts(array(
 		'number_of_posts' => '6',
 		'post_type' => 'post',
+		'box_shadow_in_lead' => '',
+		'text_shadow_in_lead' => '',
+		'meta_bg_in_lead' => '',
+		'border_radius_in_lead' => '',
 		'video_views_enabled' => 0,
 		'show_date_in_lead' => 0,
+		'border_radius' => '',
 		'show_date' => 1,
+		'show_author' => 0,
 		'order_by' => 'DESC',
 		'comments_enabled' => 0,
 		'overlay_color' => 'rgba(55,55,255,0.3)',
@@ -28,11 +34,13 @@ function niche_hero_shortcode( $atts = array() ) {
 		'post_offset' => '0',
 		'post_status' => 'publish',
 	), $atts));
-	
+	$unique_id = wp_unique_id();
+	$unique_shortcode_id = 'nh-id-' . ($unique_id - 1);
 	$s .= '<section class="niche-hero nh-'.$display.'">';
 	$s .= '<ul class="niche-posts">';
 	$recent_posts = wp_get_recent_posts(array( 'numberposts' => $number_of_posts, 'order' => $order_by, 'post_type' => $post_type, 'offset' => $post_offset, 'post_status' => $post_status));
 	foreach($recent_posts as $key => $post){
+	//$unique_id++;
 	reset($recent_posts);
 		/* Size individual post based on column selection */
 		if ($columns == '4') {
@@ -59,6 +67,32 @@ function niche_hero_shortcode( $atts = array() ) {
 		if ($comments_enabled == 1) {
 			$nh_comments = '<span class="nh-comment-number"><i class="nh-comment-icon"></i>'.get_comments_number($post['ID']).'</span>';
 		}
+		/* Customize options for leading posts meta */
+		if ($key == 0){
+			$meta_lead_styling .= '<style type="text/css">';
+				if ($meta_bg_in_lead != ''){
+				/* META & DATE BG COLOR IN LEAD */ 
+				$meta_lead_styling .= '.first-niche-post.'.$unique_shortcode_id.' .nh-date, .first-niche-post.'.$unique_shortcode_id. ' .nh-meta { background: '.$meta_bg_in_lead.' !important;}';
+				$meta_lead_styling .= '.second-niche-post.'.$unique_shortcode_id.' .nh-date, .second-niche-post.'.$unique_shortcode_id. ' .nh-meta { background: '.$meta_bg_in_lead.' !important;}';
+				$meta_lead_styling .= '.third-niche-post.'.$unique_shortcode_id.' .nh-date, .third-niche-post.'.$unique_shortcode_id. ' .nh-meta { background: '.$meta_bg_in_lead.' !important;}';				
+				} 
+				if ($text_shadow_in_lead != ''){
+				/* TEXT SHADOW IN LEAD */ 
+				$meta_lead_styling .= '.first-niche-post.'.$unique_shortcode_id.' .nh-date, .first-niche-post.'.$unique_shortcode_id.' .nh-title h4, .first-niche-post.'.$unique_shortcode_id. ' .nh-meta { text-shadow: '.$text_shadow_in_lead.' !important;}';
+				$meta_lead_styling .= '.second-niche-post.'.$unique_shortcode_id.' .nh-date, .second-niche-post.'.$unique_shortcode_id.' .nh-title h4, .second-niche-post.'.$unique_shortcode_id. ' .nh-meta { text-shadow: '.$text_shadow_in_lead.' !important;}';				
+				$meta_lead_styling .= '.third-niche-post.'.$unique_shortcode_id.' .nh-date, .third-niche-post.'.$unique_shortcode_id.' .nh-title h4, .third-niche-post.'.$unique_shortcode_id. ' .nh-meta { text-shadow: '.$text_shadow_in_lead.' !important;}';								
+				}
+				if ($border_radius_in_lead != ''){
+				$meta_lead_styling .= '.first-niche-post.'.$unique_shortcode_id.' span.niche-overlay, .first-niche-post.'.$unique_shortcode_id.' a.nh-post-thumbnail, .first-niche-post.'.$unique_shortcode_id.' a.nh-default-thumbnail div.nh-bg-placeholder { border-radius: '.$border_radius_in_lead.' !important;}';
+				$meta_lead_styling .= '.second-niche-post.'.$unique_shortcode_id.' span.niche-overlay, .second-niche-post.'.$unique_shortcode_id.' a.nh-post-thumbnail, .second-niche-post.'.$unique_shortcode_id.' a.nh-default-thumbnail div.nh-bg-placeholder { border-radius: '.$border_radius_in_lead.' !important;}';
+				$meta_lead_styling .= '.third-niche-post.'.$unique_shortcode_id.' span.niche-overlay, .third-niche-post.'.$unique_shortcode_id.' a.nh-post-thumbnail, .third-niche-post.'.$unique_shortcode_id.' a.nh-default-thumbnail div.nh-bg-placeholder { border-radius: '.$border_radius_in_lead.' !important;}';
+				}
+				if ($border_radius != ''){
+				$meta_lead_styling .= 'li.niche-post.'.$unique_shortcode_id.' .nh-post-thumbnail, li.niche-post.'.$unique_shortcode_id.' .nh-default-thumbnail { border-radius: '.$border_radius.' !important;}';
+				}				
+			$meta_lead_styling .= '</style>';
+		}
+		echo $meta_lead_styling;
 		/* Combine post meta values */
 		$nh_meta = '<div class="nh-meta">'.$nh_post_views . $nh_comments.'</div>';		
 		/* Title */
@@ -75,47 +109,56 @@ function niche_hero_shortcode( $atts = array() ) {
 		} else { 
 			$nh_post_thumbnail = '<div class="nh-bg-placeholder" style="height:'.$image_height.';background:url('.plugin_dir_url( __FILE__ ).'assets/images/260x150.png)"></div>'; 
 		}
+		/* Add all values together */
+		if (($show_author == 1) && ($display == 1 && $key != 0 && $key != 1) || $display == 3 || ($display == 2 && $key != 0 && $key != 1 && $key != 2)) {
+			$nh_detailed  = '<a href="'.get_permalink($post['ID']).'" class="nh-details"><div class="nh-avatar-box"><img src="'.get_avatar_url($post['ID'], ['size' => '300']).'"/></div><div class="nh-content">'. $nh_date . $nh_title . $nh_meta .'</div></a>';
+		} else {
+			$nh_detailed  = '<a href="'.get_permalink($post['ID']).'" class="nh-details">' . $nh_date . $nh_title . $nh_meta .'</a>';
+		}
 		/* Display section #1: Two hero boxes */
 		if ($display == '1') {
 			if($key === key($recent_posts)) { 
-				$s .= '<li class="first-niche-post"><span style="background:'.$overlay_color.'" class="niche-overlay"></span>'; 
+				$s .= '<li class="'.$unique_shortcode_id.' first-niche-post"><span style="background:'.$overlay_color.'" class="niche-overlay"></span>'; 
 			}else if($key===1){ 
-				$s .= '<li class="second-niche-post"><span style="background: '.$second_overlay_color.'" class="niche-overlay"></span>'; 
+				$s .= '<li class="'.$unique_shortcode_id.' second-niche-post"><span style="background: '.$second_overlay_color.'" class="niche-overlay"></span>'; 
 			}else{
-				$s .= '<li class="niche-post" style="min-height:'.$min_height.';width:'.$columns.'">'; 
+				$s .= '<li class="'.$unique_shortcode_id.' niche-post" style="min-height:'.$min_height.';width:'.$columns.'">'; 
 			}
 			/* Display post thumbnail or placeholder image */
 			if(has_post_thumbnail($post['ID'])){
-				$s .= '<a class="nh-post-thumbnail" style="background: url('.$nh_post_thumbnail.'); background-position: center; background-size: cover;" href="'.get_permalink($post['ID']).'">'; 
+				if (($key === key($recent_posts) || $key === 1) && $box_shadow_in_lead != '' ){
+					$s .= '<a class="nh-post-thumbnail" style="box-shadow: '.$box_shadow_in_lead.' !important;background: url('.$nh_post_thumbnail.'); background-position: center; background-size: cover;" href="'.get_permalink($post['ID']).'">'; 				
+				} else {
+					$s .= '<a class="nh-post-thumbnail" style="background: url('.$nh_post_thumbnail.'); background-position: center; background-size: cover;" href="'.get_permalink($post['ID']).'">'; 
+				}
 			}else{ 
 				$s .= '<a class="nh-default-thumbnail" href="'.get_permalink($post['ID']).'">' . $nh_post_thumbnail; 
 			}
 			$s .= '</a>';			
-			$s .= '<a href="'.get_permalink($post['ID']).'" class="nh-details">';
-			$s .= $nh_date;
-			$s .= $nh_title;
-			$s .= $nh_meta;
-			$s .= '</a>';
+			$s .= $nh_detailed;
 			$s .= '</li>';		
 		/* Display section #2: Three hero boxes */
 		} else if ($display == '2') {
 			if ($key === key($recent_posts)){
-				$s .= '<li style="width:33.3%;" class="first-niche-post"><span style="background:'.$overlay_color.'" class="niche-overlay"></span>'; 
+				$s .= '<li style="width:33.3%;" class="'.$unique_shortcode_id.' first-niche-post"><span style="background:'.$overlay_color.'" class="niche-overlay"></span>'; 
 			} else if ( $key === 1 ) { 
-				$s .= '<li style="width:33.3%;" class="second-niche-post"><span style="background:'.$second_overlay_color.'" class="niche-overlay"></span>'; 
+				$s .= '<li style="width:33.3%;" class="'.$unique_shortcode_id.' second-niche-post"><span style="background:'.$second_overlay_color.'" class="niche-overlay"></span>'; 
 			} else if ( $key === 2 ) {
-				$s .= '<li style="width:33.3%;" class="third-niche-post"><span style="background:'.$third_overlay_color.'" class="niche-overlay"></span>'; 
+				$s .= '<li style="width:33.3%;" class="'.$unique_shortcode_id.' third-niche-post"><span style="background:'.$third_overlay_color.'" class="niche-overlay"></span>'; 
 			} else { 
 				$s .= '<li class="niche-post" style="width:'.$columns.'">'; 
 			} 
-			$s .= '<a class="nh-post-thumbnail" href="'.get_permalink($post['ID']).'">';
-			$s .= $nh_post_thumbnail;
+			if(has_post_thumbnail($post['ID'])){
+				if (($key === key($recent_posts) || $key === 1 || $key === 2) && $box_shadow_in_lead != '' ){
+					$s .= '<a class="nh-post-thumbnail" style="box-shadow: '.$box_shadow_in_lead.';background: url('.$nh_post_thumbnail.'); background-position: center; background-size: cover;" href="'.get_permalink($post['ID']).'">'; 				
+				} else {
+					$s .= '<a class="nh-post-thumbnail" style="background: url('.$nh_post_thumbnail.'); background-position: center; background-size: cover;" href="'.get_permalink($post['ID']).'">'; 
+				}
+			}else{ 
+				$s .= '<a class="nh-default-thumbnail" href="'.get_permalink($post['ID']).'">' . $nh_post_thumbnail; 
+			}
 			$s .= '</a>';
-			$s .= '<a href="'.get_permalink($post['ID']).'" class="nh-details">';
-			$s .= $nh_date;
-			$s .= $nh_title;
-			$s .= $nh_meta;
-			$s .= '</a>';
+			$s .= $nh_detailed;
 			$s .= '</li>';	
 			/* Display section #3: Hero list */
 			} else if ($display == '3') {
@@ -126,22 +169,14 @@ function niche_hero_shortcode( $atts = array() ) {
 							}else{
 								$s .= '<a class="nh-default-thumbnail" href="'.get_permalink($post['ID']).'">' . $nh_post_thumbnail.'</a>';
 							}						
-							$s .= '<a class="nh-details">';
-							$s .= $nh_date;
-							$s .= $nh_title;
-							$s .= $nh_meta;
-							$s .= '</a>';
+							$s .= $nh_detailed;
 						}else{
 							if (has_post_thumbnail($post['ID'])){
 								$s .= '<a class="nh-post-thumbnail" style="padding-top: '.$image_height.'; height:'.$image_height.'; background: url('.$nh_post_thumbnail.'); background-position: center; background-size: cover;" href="'.get_permalink($post['ID']).'"></a>'; 
 							} else { 
 								$s .= '<a class="nh-default-thumbnail" href="'.get_permalink($post['ID']).'">' . $nh_post_thumbnail.'</a>'; 
 							}
-							$s .= '<a class="nh-details">';
-							$s .= $nh_date;
-							$s .= $nh_title;
-							$s .= $nh_meta;
-							$s .= '</a>';
+							$s .= $nh_detailed;
 						}
 					$s .= '</li>';						
 			}
@@ -170,11 +205,24 @@ function niche_hero_add_stylesheet() {
 
 function niche_hero_inline_css() {
 	    $google_font = get_theme_mod( 'nh_select_google_font' );
-	    $custom_css = "<style type=\"text/css\">section.niche-hero li.niche-post h4,
-					   section.niche-hero li.niche-post span.details,
-					   section.niche-hero li.first-niche-post span.details,
-					   section.niche-hero li.second-niche-post span.details,
-					   section.niche-hero li.third-niche-post span.details { font-family: $google_font !important; }</style>";
+	    $custom_css = "<style type=\"text/css\">
+		li.niche-post div.nh-date,
+		li.first-niche-post div.nh-date,
+		li.second-niche-post div.nh-date,
+		li.third-niche-post div.nh-date,
+		li.niche-post span.nh-video-views,
+		li.first-niche-post span.nh-video-views,
+		li.second-niche-post span.nh-video-views,
+		li.third-niche-post span.nh-video-views,
+		li.first-niche-post div.nh-title h4,
+		li.niche-post span.nh-comment-number,
+		li.first-niche-post span.nh-comment-number,
+		li.second-niche-post div.nh-title h4,
+		li.second-niche-post span.nh-comment-number,
+		li.third-niche-post div.nh-title h4,
+		li.third-niche-post span.nh-comment-number,
+		li.niche-post div.nh-title h4
+		{ font-family: $google_font !important; }</style>";
         echo $custom_css;
 }
 add_action( 'wp_head', 'niche_hero_inline_css' );
@@ -216,7 +264,7 @@ function niche_hero_plugin_customizer( $wp_customize ) {
 		$wp_customize->add_setting(
 			'nh_select_google_font',
 			array(
-				'default' => 'roboto',
+				'default' => 'Roboto',
 			)
 		);
 		$wp_customize->add_control(
